b2aa9e0b5c1bb26c4c4b6cd9158c23910b9d9193
Jahan <34219176+JahanUoL@users.noreply.github.com>
2022-01-25 13:28:51 +0000

    using firebase users now

--- backend/src/services/telegramService.ts
+++ backend/src/services/telegramService.ts
@@ -7,11 +7,12 @@ const firebaseService = require("../services/firebaseService");
 const api = new TG({ token: process.env.TELEGRAM_API });
 
 async function sendPhotosToUsers(items: Item[]) {
-    // const users = await firebaseService.getUsers(); 
-    const users = [{
-        telegramId: process.env.CHAT_ID_JAHAN,
-        fullName: 'Jahan',
-    }];
+    const users = await firebaseService.getUsers();
+
+    // const users = [{
+    //     telegramId: process.env.CHAT_ID_JAHAN,
+    //     fullName: 'Jahan',
+    // }];
 
     console.log('users: ', users);
     items.forEach((i) => { // .itemName, .wasPrice, .nowPrice, .discount, .url, .imageUrl, .sizes
