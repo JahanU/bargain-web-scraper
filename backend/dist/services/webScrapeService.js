db17a1db68614f55d65c70079bfced43e39385b1
Jahan <34219176+JahanUoL@users.noreply.github.com>
2022-02-21 19:58:11 +0000

    cache reset freq

--- backend/dist/services/webScrapeService.js
+++ backend/dist/services/webScrapeService.js
@@ -20,7 +20,7 @@ let discountLimit = 10; // item discount must be greater than this value
 function main() {
     startScraping();
     setInterval(startScraping, 120 * 1000); // every 2 minutes
-    setInterval(resetCache, 7200 * 1000); // every 2 hours
+    setInterval(resetCache, 18000 * 1000); // every 5 hours
 }
 function startScraping() {
     return __awaiter(this, void 0, void 0, function* () {
