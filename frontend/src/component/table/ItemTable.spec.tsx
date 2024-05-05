c77274ee5fc034a163b38361283f4831616eeff3
Jahan <34219176+JahanUoL@users.noreply.github.com>
2024-05-05 15:24:20 +0100

    itemTable spec

---
+++ frontend/src/component/table/ItemTable.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import ItemTable from './ItemTable';
+import '@testing-library/jest-dom'
+
+// Mock data for items
+const mockItems: any[] = [
+    { id: 1, name: 'Item 1', imageUrl: '...', },
+    { id: 2, name: 'Item 2', imageUrl: '...', },
+];
+
+describe('ItemTable', () => {
+    it('renders "Loading" message while isLoading is true', () => {
+        render(<ItemTable items={[]} isLoading={true} />);
+        const loadingText = screen.getByText('Loading');
+        expect(loadingText).toBeInTheDocument();
+    });
+
+    it('renders "No Items Found" message when items is empty and isLoading is false', () => {
+        render(<ItemTable items={[]} isLoading={false} />);
+        const noItemsText = screen.getByText('Sorry!');
+        expect(noItemsText).toBeInTheDocument();
+    });
+
+
+    it('renders ItemCard components for each item', () => {
+        render(<ItemTable items={mockItems} isLoading={false} />);
+        const header = screen.getByText('Latest Picks');
+        expect(header).toBeInTheDocument();
+        const itemsLength = screen.getByText('(2)');
+        expect(itemsLength).toBeInTheDocument();
+    });
+});
