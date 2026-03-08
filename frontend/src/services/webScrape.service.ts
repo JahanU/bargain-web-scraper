a387a39fa1c7190dbe4513da37a12d248ce8d404
Jahan <Jahanulhaque@gmail.com>
2026-03-08 23:11:38 +0000

    feat: Dynamically set the frontend API base URL based on environment

--- frontend/src/services/webScrape.service.ts
+++ frontend/src/services/webScrape.service.ts
@@ -1,15 +1,16 @@
 import Item from '../interfaces/Item';
-// import mockData from './mock-data.json'; // Import the data 
 
 const getItemsService = async (): Promise<Item[]> => {
-    // const url = 'https://bargain-scraper-710da0f863f3.herokuapp.com/webscrape/getBestDeals';
-    const url = 'https://bargain-web-scraper-8bbn.onrender.com/webscrape/getBestDeals';
-    // const url = 'http://localhost:8000/webscrape/getBestDeals';
+    const isLocal = process.env.NODE_ENV === 'development';
+    const baseUrl = isLocal
+        ? 'http://localhost:8000'
+        : 'https://bargain-web-scraper-8bbn.onrender.com';
+
+    const url = `${baseUrl}/webscrape/getBestDeals`;
+
     const response = await fetch(url);
     const data = await response.json();
     return data;
 };
-
-// const getItemsService = async () => mockData;
 
 export { getItemsService };
